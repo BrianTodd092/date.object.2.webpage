6043eb057717c11b009359fc6f32ad8b2b357d55
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2021-02-11 12:54:44 +0000

    Add files via upload
    JavaScript new Date() on two web page, note order of Script.

---
+++ contact.js
@@ -0,0 +1,40 @@
+var date = new Date();
+var n = date.toLocaleString('en-US');
+document.getElementsByClassName('p')[0].innerHTML = n;
+document.getElementById('h').addEventListener('click', clicked, false);
+
+
+
+
+function clicked(){
+
+
+window.location="index.html";
+
+}
+
+
+
+document.getElementsByClassName('p')[1].innerHTML = n;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
